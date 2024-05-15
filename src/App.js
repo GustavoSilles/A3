bc7fbc014284dd14070eadfd77cee259750c41cd
Gustavo Drohobeczky Silles <92472220+GustavoSilles@users.noreply.github.com>
2024-05-15 12:42:58 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Projetinho A3
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=SnyQhuQvALQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Coisas da vida
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
