e584d5085be6244bfd324ed5e4526ed8c3664659
Gabriel da Fonseca Meira <gabriel.gfm@outlook.com>
2024-05-26 14:35:37 -0300

    primeira definição da conexão com a api no PowerBI

--- src/components/Home2/home2.js
+++ src/components/Home2/home2.js
@@ -1,14 +1,56 @@
-import React from "react";
 import "./home2Styles.css";
+import * as pbi from 'powerbi-client';
+import React, {useEffect, useRef} from 'react';
 // import { Link } from "react-router-dom";
+
+const PowerBIReport = () => {
+  const reportRef = useRef(null);
+
+  useEffect(() => {
+      const embedReport = async () => {
+          const accesToken = 'TOKEN QUE A KAUANY VAI GERAR';
+          const embedUrl = 'URL QUE A KAUANY VAI GERAR';
+          const embedConfig = {
+              type: 'report',
+              id: 'ID QUE A KAUANY VAI GERAR',
+              embedUrl: embedUrl,
+              accesToken: accesToken,
+              tokenType: pbi.models.TokenType.Embed,
+              settings: {
+                  panes: {
+                      filters: {
+                          expanded: false,
+                          visible: false,
+                      },
+                  },
+              },
+          };
+
+          const powerbi = new pbi.service.Service(
+              pbi.factories.hpmFactory, 
+              pbi.factories.wpmpFactory, 
+              pbi.factories.routerFactory
+          );
+
+          const report = powerbi.embed(
+              reportRef.current, 
+              embedConfig
+          );
+      };
+
+      embedReport();
+  }, []);
+
+  //return <div ref={reportRef} style={{height: '600px'}} />;
+};
 
 const Home2 = () => {
   return (
     <div className="home2">
       <div className="container">
-        
-        </div>
+        <div ref={reportRef} style={{height: '600px'}} />
       </div>
+    </div>
   
   );
 };
--- src/components/Home3/home3.js
+++ src/components/Home3/home3.js
@@ -1,11 +1,53 @@
-import React from "react";
 import "./home3Styles.css";
+import * as pbi from 'powerbi-client';
+import React, {useEffect, useRef} from 'react';
+
+const PowerBIReport = () => {
+  const reportRef = useRef(null);
+
+  useEffect(() => {
+      const embedReport = async () => {
+          const accesToken = 'TOKEN QUE A KAUANY VAI GERAR';
+          const embedUrl = 'URL QUE A KAUANY VAI GERAR';
+          const embedConfig = {
+              type: 'report',
+              id: 'ID QUE A KAUANY VAI GERAR',
+              embedUrl: embedUrl,
+              accesToken: accesToken,
+              tokenType: pbi.models.TokenType.Embed,
+              settings: {
+                  panes: {
+                      filters: {
+                          expanded: false,
+                          visible: false,
+                      },
+                  },
+              },
+          };
+
+          const powerbi = new pbi.service.Service(
+              pbi.factories.hpmFactory, 
+              pbi.factories.wpmpFactory, 
+              pbi.factories.routerFactory
+          );
+
+          const report = powerbi.embed(
+              reportRef.current, 
+              embedConfig
+          );
+      };
+
+      embedReport();
+  }, []);
+
+  //return <div ref={reportRef} style={{height: '600px'}} />;
+};
 
 const Home3 = () => {
   return (
     <div className="home3">
       <div className="container">
-       
+        <div ref={reportRef} style={{height: '600px'}} />
       </div>
     </div>
   );
