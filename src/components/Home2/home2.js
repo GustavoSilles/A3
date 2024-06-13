aba7c88070ffbfdedcb01b679608a3c35d061f5e
GustavoSilles <sillesgustavo05@gmail.com>
2024-06-13 12:21:43 -0300

    testes

--- src/components/Home2/home2.js
+++ src/components/Home2/home2.js
@@ -1,23 +1,29 @@
 import "./home2Styles.css";
 import React from 'react';
 
+
 const Home2 = () => {
+
   const embedUrl = "https://app.powerbi.com/view?r=eyJrIjoiOWU2MzQ0ZTEtNWI0OS00YmUzLTk3OWUtZWYwZDVlMmE4ODc1IiwidCI6IjEzNDllMzA2LWU2MGItNGU5OS04NGJiLWQyOTAyMTkzMTBkOSJ9";
 
   return (
-    <div id="home2" className="home2">
+    <div className="home2">
       <div className="container">
         <div className="painel">
-          <h1 className="titulodash">Dashboards Informativos</h1>
-          <iframe  className="iframe"
+        <h1 className="titulodash">Dashboards Informativos</h1>
+          <iframe className="iframe"
               title="Power BI Report"
               src={embedUrl}
               allowFullScreen={true}
           ></iframe> 
-        </div>
+          </div>
       </div>
     </div>
+  
   );
 };
 
 export default Home2;
+
+
+
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,65 +1,45 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
 import "./navbarStyles.css";
-
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const toggleNav = () => setNav(!nav);
-
-  const handleNavClick = (to) => {
-    const scrollToElement = () => {
-      const element = document.getElementById(to);
-      if (element) {
-        element.scrollIntoView();
-      }
-    };
-
-    if (location.pathname !== "/") {
-      navigate("/");
-      setTimeout(() => {
-        const interval = setInterval(() => {
-          if (document.getElementById(to)) {
-            clearInterval(interval);
-            scrollToElement();
-          }
-        }, 100);
-      }, 100);
-    } else {
-      scrollToElement();
-    }
-
+  
+  
+  const handleLinkClick = () => {
     setNav(false);
+  };
+
+  
+  const handleNav = () => {
+    setNav(!nav);
   };
 
   return (
     <div name="top">
       <div className="navbar">
-        <div className="logo" onClick={() => window.scrollTo({ top: 0 })}>
-          <h1 className="textlogo">InfoViolência</h1>
+        <div className="logo">
+          <h1 className="textlogo">Violencia</h1>
         </div>
+
         <ul className={nav ? "nav-menu active" : "nav-menu"}>
           <li>
-            <RouterLink className="l1" to="/" onClick={() => handleNavClick("home")}>
+            <a className="l1" href="#home" onClick={handleLinkClick}>
               introdução
-            </RouterLink>
+            </a>
           </li>
           <li>
-            <RouterLink className="l2" to="/" onClick={() => handleNavClick("home2")}>
+            <a className="l2" href="#home2" onClick={handleLinkClick}>
               Dashboards
-            </RouterLink>
+            </a>
           </li>
           <li>
-            <RouterLink className="l3" to="/" onClick={() => handleNavClick("home3")}>
+            <a className="l3" href="#home3" onClick={handleLinkClick}>
               Procure ajuda
-            </RouterLink>
+            </a>
           </li>
         </ul>
-        <div className="hamburger" onClick={toggleNav}>
-          {nav ? <FaTimes className="icon" /> : <FaBars className="icon" />}
+        <div className="hamburger" onClick={handleNav}>
+          {!nav ? <FaBars className="icon" /> : <FaTimes className="icon" />}
         </div>
       </div>
     </div>
