dcbfecb02b27ac1ccae2ea888447c0773ba53bd1
GustavoSilles <sillesgustavo05@gmail.com>
2024-06-14 15:27:54 -0300

    tudo 100% pronto

--- src/components/Leiamais/leiamais.js
+++ src/components/Leiamais/leiamais.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PiFlag } from 'react-icons/pi';
-import { TbDiamond, TbPointFilled } from 'react-icons/tb'; // Importar TbPointFilled
+import { TbDiamond, TbPointFilled } from 'react-icons/tb';
 import { FaRegEye } from 'react-icons/fa';
 import { IoMdClose } from 'react-icons/io';
-import './leiamais.css'; // Importing the existing stylesheet for consistent styles
-
+import './leiamais.css';
 
 const Leiamais = ({ closeModal }) => {
+  useEffect(() => {
+    const adjustModalOverlayHeight = () => {
+      const modalOverlay = document.querySelector('.modal-overlay');
+      if (modalOverlay) {
+        modalOverlay.style.height = `${window.innerHeight}px`;
+      }
+    };
+
+    // Adjust the height on initial load and on resize
+    window.addEventListener('resize', adjustModalOverlayHeight);
+    window.addEventListener('load', adjustModalOverlayHeight);
+    // Call the function immediately to set the initial height
+    adjustModalOverlayHeight();
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('resize', adjustModalOverlayHeight);
+      window.removeEventListener('load', adjustModalOverlayHeight);
+    };
+  }, []);
+
   const handleBackgroundClick = (e) => {
     if (e.target.className === 'modal-overlay') {
       closeModal();
@@ -33,26 +53,25 @@ const Leiamais = ({ closeModal }) => {
           <p>Enfrentar a violência no Brasil por meio de mecanismos de conscientização, informação e mobilização. O objetivo deste site é fornecer dados e informações para conscientizar, informar e mobilizar as pessoas em relação a esse problema nacional. Aqui você encontrará dashboards úteis para suas pesquisas sobre o assunto.</p>
         </div>
         <div className="content-section">
-  <div className="icon-wrapper">
-    <TbDiamond className="icon" />
-  </div>
-  <h3>Valores</h3>
-  <div className="values-list">
-    <div className="value-item"><TbPointFilled className="iconli" /> Transparência</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Compromisso Social</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Confiabilidade</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Educação</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Ética</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Justiça</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Honestidade</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Solidariedade</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Comprometimento</div>
-    <div className="value-item"><TbPointFilled className="iconli" /> Despreconceito</div>
-  </div>
-</div>
-
+          <div className="icon-wrapper">
+            <TbDiamond className="icon" />
+          </div>
+          <h3>Valores</h3>
+          <div className="values-list">
+            <div className="value-item"><TbPointFilled className="iconli" /> Transparência</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Compromisso Social</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Confiabilidade</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Educação</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Ética</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Justiça</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Honestidade</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Solidariedade</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Comprometimento</div>
+            <div className="value-item"><TbPointFilled className="iconli" /> Despreconceito</div>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
   );
 };
 
